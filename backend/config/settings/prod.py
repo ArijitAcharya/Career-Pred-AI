a12b65ada616f520d6caaa761f9192abbd2473ab
Ariji <ariji@example.com>
2026-02-27 00:38:29 +0530

    Production DB config: Added PostgreSQL via DATABASE_URL for Render

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -1,9 +1,14 @@
 from .base import *
 import os
+import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]  # temporary for deployment
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
